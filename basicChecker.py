ce15b4c87068cdcd17059a489caf8183866681c4
Jacky Zhu <jackyzhu@berkeley.edu>
2016-12-30 08:58:30 -0800

    implemented BeautifulSoup4's encoding detection and lxml's HTML parser, now finds links within websites

--- basicChecker.py
+++ basicChecker.py
@@ -2,7 +2,9 @@ from urllib.request import Request, urlopen  # Modules used to access websites w
 from urllib.error import URLError, HTTPError  # Modules used to deal with errors with accessing websites
 from http.client import IncompleteRead, BadStatusLine  #
 from ssl import CertificateError  #
+from lxml import etree
 from time import gmtime, strftime
+from bs4 import UnicodeDammit
 import openpyxl  # library for pulling data from excel files
 import csv  # library for outputting csv files
 import ssl  # deals with SSL Certificate Error
@@ -11,11 +13,11 @@ import os
 import sys
 
 
-spreadsheet = openpyxl.load_workbook('Test.xlsx')
-data = spreadsheet.get_sheet_by_name('Sheet1')
+# spreadsheet = openpyxl.load_workbook('Test.xlsx')
+# data = spreadsheet.get_sheet_by_name('Sheet1')
 
-# spreadsheet = openpyxl.load_workbook('2015 CloudShare - December Final.xlsx')  # pulls data directly from excel file
-# data = spreadsheet.get_sheet_by_name('Data')  # specifies which sheet to pull data from
+spreadsheet = openpyxl.load_workbook('2015 CloudShare - December Final.xlsx')  # pulls data directly from excel file
+data = spreadsheet.get_sheet_by_name('Data')  # specifies which sheet to pull data from
 
 
 # checks if a text contains a specified character
@@ -48,15 +50,25 @@ if checkDirectoryExistence():
 
 os.makedirs(savePath) #creates a new directory to store html files
 
-websites = []  # stores all the URLs
+
+class Website:
+    def __init__(self, URL, provider, i, depth):
+        self.URL = URL
+        self.provider = provider
+        self.row = i
+        self.depth = depth
+
+
+websites = []  # stores all the URLs from the excel file
 for i in range(2, data.max_row):
-    webpage = data.cell(row=i, column=3).value
-    if webpage != None: #case for when no website is given
-        if webpage[0:4] != 'http' and webpage[0:3] != 'See':  # appends http heading if not already present and checks if the provider is a subordinate of another provider
-            webpage = 'http://' + webpage
+    URL = data.cell(row=i, column=3).value
+    if URL != None: #case for when no website is given
+        if URL[0:4] != 'http' and URL[0:3] != 'See':  # appends http heading if not already present and checks if the provider is a subordinate of another provider
+            URL = 'http://' + URL
     else:
-        webpage = ' '
-    websites.append(webpage)
+        URL = ' '
+    website = Website(URL, data.cell(row=i, column=2).value, i, 0)
+    websites.append(website)
 
 hdr = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
@@ -74,76 +86,172 @@ ErrorArray = []  # stores all the websites that return an error (later written i
 ErrorArray.append(("Row", "Provider", "Error Message", "Error Code", "Website"))
 
 NewWebsites = []
-NewWebsites.append(("Provider","Websites"))
-for i in range(len(websites)):  # iterates through every website
+NewWebsites.append(("Row", "Provider","Websites"))
 
-    if websites[i] == ' ': #when no website is given, skip the loop
+def findOtherLinks(websites, website, html):
+    for link in html.xpath('//a/@href'):  # find all links on webpage and add to new list
+        # if link[0:3] == "www":
+        #     link = "http://" + link
+        # if link[0:4] != 'http':
+        #     continue
+        NewWebsites.append((website.row, website.provider, link))
+        # websites.append(Website(link, website.provider, website.row, 1))
+    # return websites
+
+def createHTMLFile(fileName, savePath, webpage):
+    if checkForCharacter('/', fileName):
+        fileName = replaceAll('/', '_', fileName)
+    newFile = open(os.path.join(savePath, fileName + '.html'), 'w')
+    newFile.write(webpage)
+    newFile.close()
+
+def goToURL(websites, website):
+    context = ssl._create_unverified_context()  # bypasses SSL Certificate Verfication (probably not a good idea, but it got more sites to work)
+    req = Request(website.URL,
+                  headers=hdr)  # changing the header to Mozilla/5.0 prevents some webscraper blocking techniques
+    try:
+
+        response = urlopen(req, context=context).read()  # opens the URL
+
+        # Various errors have popped up, so I set up exceptions to catch them.
+        # There might be a better way to do this.
+    except ConnectionResetError:
+        print(website.provider, '\'s ', 'server didn\'t send data')
+        ErrorArray.append((website.row, website.provider, 'Server didn\'t send data', ' ',
+                           website.URL))
+    except HTTPError as e:
+        print(website.provider, '\'s ', 'server couldn\'t fulfill the request.')
+        ErrorArray.append((website.row, website.provider, 'HTTPError', e.code,
+                           website.URL))
+        print('Error code: ', e.code)
+    except URLError as e:
+        print('We failed to reach', website.provider, '\'s ', 'server.')
+        ErrorArray.append((website.row, website.provider, 'URLError ', repr(e.reason),
+                           website.URL))
+        print('Reason: ', e.reason)
+    except CertificateError:
+        print("SSL Certificate Error with ", website.provider)
+        ErrorArray.append((website.row, website.provider, 'SSL Certificate Error', ' ',
+                           website.URL))
+    except IncompleteRead as e:
+        print("IncompleteRead Error with ", website.provider)
+        ErrorArray.append((website.row, website.provider, 'Incomplete Read Error', ' ',
+                           website.URL))
+    except BadStatusLine:
+        print("BadStatusLine Error with ", website.provider)
+        ErrorArray.append(
+            (website.row, website.provider, 'Bad Status Line', ' ', website.URL))
+    else:  # if no error occurs
+        try:
+            saveWebPage = UnicodeDammit(response).unicode_markup # data (in bytes) from opening URL is decoded into String format
+            html = lxml.html.fromstring(response)
+
+        except UnicodeDecodeError:  # some errors have occurred when decoding characters from non-English languages
+            print("Unicode Decode Error with ", website.provider)
+            ErrorArray.append(
+                (website.row, website.provider, 'Decode Error', ' ', website.URL))
+        except etree.ParserError:
+            print("ParserError with ", website.provider)
+            ErrorArray.append((website.row, website.provider, "Parser Error", " ", website.URL))
+        except etree.XMLSyntaxError:
+            print("XMLSyntaxError with ", website.provider)
+            ErrorArray.append((website.row, website.provider, "XML Synxtax Error", " ", website.URL))
+        else:
+            if website.depth == 0:
+                # websites = findOtherLinks(websites, website, html)
+                findOtherLinks(websites, website, html)
+
+            #create an html file of the website
+            WorkingArray.append((website.row, website.provider, website.URL))
+            fileName = website.URL
+            createHTMLFile(fileName, savePath, saveWebPage)
+            print(website.provider, ' is working fine')  # write all data in ErrorArray into a CSV file
+    # return websites
+
+
+
+for website in websites:
+    if website.URL == ' ': #when no website is given, skip the loop
         continue;
-    if (websites[i][0:3] == 'See'):  # checks for subordinate case first and ends current loop if true
+    elif (website.URL[0:3] == 'See'):  # checks for subordinate case first and ends current loop if true
         print("Duplicate")
         ErrorArray.append(
-            (i + 2, data.cell(row=i + 2, column=2).value, 'Duplicate', ' ', data.cell(row=i + 2, column=3).value))
+            (website.row, website.provider, 'Duplicate', ' ', website.URL))
     else:
-
-        context = ssl._create_unverified_context()  # bypasses SSL Certificate Verfication (proabably not a good idea, but it got more sites to work)
-        req = Request(websites[i],
-                      headers=hdr)  # changing the header to Mozilla/5.0 prevents some webscraper blocking techniques
-        try:
-
-            response = urlopen(req, context=context).read()  # opens the URL
+        # websites = goToURL(websites, website)
+        goToURL(websites, website)
 
 
-            # Various errors have popped up, so I set up exceptions to catch them.
-            # Should be a better way to do this.
-        except ConnectionResetError:
-            print(data.cell(row=i + 2, column=2).value, '\'s ', 'server didn\'t send data')
-            ErrorArray.append((i + 2, data.cell(row=i + 2, column=2).value, 'Server didn\'t send data', ' ',
-                               data.cell(row=i + 2, column=3).value))
-        except HTTPError as e:
-            print(data.cell(row=i + 2, column=2).value, '\'s ', 'server couldn\'t fulfill the request.')
-            ErrorArray.append((i + 2, data.cell(row=i + 2, column=2).value, 'HTTPError', e.code,
-                               data.cell(row=i + 2, column=3).value))
-            print('Error code: ', e.code)
-        except URLError as e:
-            print('We failed to reach', data.cell(row=i + 2, column=2).value, '\'s ', 'server.')
-            ErrorArray.append((i + 2, data.cell(row=i + 2, column=2).value, 'URLError ', repr(e.reason),
-                               data.cell(row=i + 2, column=3).value))
-            print('Reason: ', e.reason)
-        except CertificateError:
-            print("SSL Certificate Error with ", data.cell(row=i + 2, column=2).value)
-            ErrorArray.append((i + 2, data.cell(row=i + 2, column=2).value, 'SSL Certificate Error', ' ',
-                               data.cell(row=i + 2, column=3).value))
-        except IncompleteRead as e:
-            print("IncompleteRead Error with ", data.cell(row=i + 2, column=2).value)
-            ErrorArray.append((i + 2, data.cell(row=i + 2, column=2).value, 'Incomplete Read Error', ' ',
-                               data.cell(row=i + 2, column=3).value))
-        except BadStatusLine:
-            print("BadStatusLine Error with ", data.cell(row=i + 2, column=2).value)
-            ErrorArray.append(
-                (i + 2, data.cell(row=i + 2, column=2).value, 'Bad Status Line', ' ', data.cell(row=i + 2, column=3).value))
-        else:  # if no error occurs
-            try:
-                html = lxml.html.fromstring(response)
-                saveWebPage = response.decode() # data (in bytes) from opening URL is decoded into String format
-            except UnicodeDecodeError:  # some errors have occurred when decoding characters from non-English languages
-                print("Unicode Decode Error with ", data.cell(row=i + 2, column=2).value)
-                ErrorArray.append(
-                    (i + 2, data.cell(row=i + 2, column=2).value, 'Decode Error', ' ', data.cell(row=i + 2, column=3).value))
-            else:
 
-                for link in html.xpath('//a/@href'):
-                    NewWebsites.append((data.cell(row=i + 2, column=2).value, link))
-
-
-                #create a text file to store html file of the website
-                WorkingArray.append((i + 2, data.cell(row=i + 2, column=2).value, data.cell(row=i + 2, column=3).value))
-                fileName = data.cell(row=i + 2, column=2).value
-                if checkForCharacter('/', fileName):
-                    fileName = replaceAll('/', '_', fileName)
-                newFile = open(os.path.join(savePath, fileName + '.html'), 'w')
-                newFile.write(saveWebPage)
-                newFile.close()
-                print(data.cell(row=i + 2, column=2).value, ' is working fine')  # write all data in ErrorArray into a CSV file
+# for i in range(len(websites)):  # iterates through every website
+#
+#     if websites[i] == ' ': #when no website is given, skip the loop
+#         continue;
+#     if (websites[i][0:3] == 'See'):  # checks for subordinate case first and ends current loop if true
+#         print("Duplicate")
+#         ErrorArray.append(
+#             (i + 2, data.cell(row=i + 2, column=2).value, 'Duplicate', ' ', data.cell(row=i + 2, column=3).value))
+#     else:
+#
+#         context = ssl._create_unverified_context()  # bypasses SSL Certificate Verfication (proabably not a good idea, but it got more sites to work)
+#         req = Request(websites[i],
+#                       headers=hdr)  # changing the header prevents some webscraper-blocking techniques
+#         try:
+#
+#             response = urlopen(req, context=context).read()  # opens the URL
+#
+#
+#             # Various errors have popped up, so I set up exceptions to catch them.
+#             # Should be a better way to do this.
+#         except ConnectionResetError:
+#             print(data.cell(row=i + 2, column=2).value, '\'s ', 'server didn\'t send data')
+#             ErrorArray.append((i + 2, data.cell(row=i + 2, column=2).value, 'Server didn\'t send data', ' ',
+#                                data.cell(row=i + 2, column=3).value))
+#         except HTTPError as e:
+#             print(data.cell(row=i + 2, column=2).value, '\'s ', 'server couldn\'t fulfill the request.')
+#             ErrorArray.append((i + 2, data.cell(row=i + 2, column=2).value, 'HTTPError', e.code,
+#                                data.cell(row=i + 2, column=3).value))
+#             print('Error code: ', e.code)
+#         except URLError as e:
+#             print('We failed to reach', data.cell(row=i + 2, column=2).value, '\'s ', 'server.')
+#             ErrorArray.append((i + 2, data.cell(row=i + 2, column=2).value, 'URLError ', repr(e.reason),
+#                                data.cell(row=i + 2, column=3).value))
+#             print('Reason: ', e.reason)
+#         except CertificateError:
+#             print("SSL Certificate Error with ", data.cell(row=i + 2, column=2).value)
+#             ErrorArray.append((i + 2, data.cell(row=i + 2, column=2).value, 'SSL Certificate Error', ' ',
+#                                data.cell(row=i + 2, column=3).value))
+#         except IncompleteRead as e:
+#             print("IncompleteRead Error with ", data.cell(row=i + 2, column=2).value)
+#             ErrorArray.append((i + 2, data.cell(row=i + 2, column=2).value, 'Incomplete Read Error', ' ',
+#                                data.cell(row=i + 2, column=3).value))
+#         except BadStatusLine:
+#             print("BadStatusLine Error with ", data.cell(row=i + 2, column=2).value)
+#             ErrorArray.append(
+#                 (i + 2, data.cell(row=i + 2, column=2).value, 'Bad Status Line', ' ', data.cell(row=i + 2, column=3).value))
+#         else:  # if no error occurs
+#             try:
+#                 html = lxml.html.fromstring(response) #converts data from opening URL into htmlElement type
+#                 saveWebPage = response.decode() # data (in bytes) from opening URL is decoded into String format
+#             except UnicodeDecodeError:  # some errors have occurred when decoding characters from non-English languages
+#                 print("Unicode Decode Error with ", data.cell(row=i + 2, column=2).value)
+#                 ErrorArray.append(
+#                     (i + 2, data.cell(row=i + 2, column=2).value, 'Decode Error', ' ', data.cell(row=i + 2, column=3).value))
+#             else:
+#
+#                 for link in html.xpath('//a/@href'): #find all links on webpage and add to new list
+#                     NewWebsites.append((data.cell(row=i + 2, column=2).value, link))
+#
+#
+#                 #create a text file to store html file of the website
+#                 WorkingArray.append((i + 2, data.cell(row=i + 2, column=2).value, data.cell(row=i + 2, column=3).value))
+#                 fileName = data.cell(row=i + 2, column=2).value
+#                 if checkForCharacter('/', fileName):
+#                     fileName = replaceAll('/', '_', fileName)
+#                 newFile = open(os.path.join(savePath, fileName + '.html'), 'w')
+#                 newFile.write(saveWebPage)
+#                 newFile.close()
+#                 print(data.cell(row=i + 2, column=2).value, ' is working fine')  # write all data in ErrorArray into a CSV file
 
 with open("Nonworking_Websites" + strftime("%Y-%m-%d %H_%M_%S", gmtime()) + ".csv", "w") as output:
     writer = csv.writer(output, lineterminator='\n')
