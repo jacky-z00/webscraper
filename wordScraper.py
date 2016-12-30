3082d4215c8eef6f45280fd489565b2ec3627015
jwong6022 <jwong6022@berkeley.edu>
2016-12-30 11:04:37 -0800

    Update wordScraper.py

--- wordScraper.py
+++ wordScraper.py
@@ -1,15 +1,17 @@
-from urllib.request import urlopen
-import urllib.error
-import ssl
 from bs4 import BeautifulSoup
-import re
 import collections
+from http.client import IncompleteRead, BadStatusLine
+import ssl
+import urllib.request 
+import re
+
 
 def word_index(site):
     
     # Words we don't care about
-    useless_words = ['the', 'contact', 'us', 'and', 'subscribe', 'to', 'on', 'a', 'our', 'visit', 'all', 'rights', 'reserved',
-                'your', 'for', 'more', 'read', 'their', 'with', 'every', 'you', 'what', 'why', 'how', 'new']  
+    useless_words = ['the', 'contact', 'us', 'and', 'subscribe', 'our', 'visit', 'all', 'rights', 'reserved',
+                'your', 'for', 'more', 'read', 'their', 'with', 'every', 'you', 'what', 'why', 'how', 'new', 'are', 'that', 'from'
+                    'get', 'can' 'see', 'use', 'about', 'have' 'this','now', 'has', 'will', 'not', 'its']  
     
     #fix certain links
     def http_to_site(site):
@@ -17,6 +19,33 @@ def word_index(site):
             site = 'http://' + site
         return site
     site = http_to_site(site)
+    # Header to bypass firewalls
+    
+    hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+    'Accept-Encoding': 'none',
+    'Accept-Language': 'en-US,en;q=0.8',
+    'Connection': 'keep-alive'}
+
+    req = urllib.request.Request(site, headers=hdr) # adds headers to request website
+
+    context = ssl._create_unverified_context() # allow access of unverified certificate websites
+    
+    try:
+         page = urlopen(req, context = context) # opens site
+    except ConnectionResetError:
+         return collections.Counter()
+    except urllib.error.URLError as e:
+        return collections.Counter()
+    except HTTPError as e:
+        return collections.Counter()
+    except http.client.IncompleteRead as e:
+        return collections.Counter()
+    except http.client.BadStatusLine:
+        return collections.Counter()
+    except ssl.CertificateError:
+        return collections.Counter()
     
     # ignore words shorter than 3 letters, numbers, and words in useless_words
     def validate_word(word):
@@ -26,21 +55,6 @@ def word_index(site):
 # Function to scrape text
     def text_scraper(site):
         
-    # Header to bypass firewalls
-    
-        hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-       'Accept-Encoding': 'none',
-       'Accept-Language': 'en-US,en;q=0.8',
-       'Connection': 'keep-alive'}
-    
-        req = urllib.request.Request(site, headers=hdr) # adds headers to request website
-        
-        context = ssl._create_unverified_context() # allow access of unverified certificate websites
-
-        page = urlopen(req, context = context) # opens site
-
         soup = BeautifulSoup(page,"lxml") # get html from site
 
     # kill all script and style elements
@@ -59,7 +73,7 @@ def word_index(site):
         
         return text
 
-    words_index =  re.findall(r'\w+', text_scrapper(site).lower()) # find only words and make them lower case
+    words_index =  re.findall(r'\w+', text_scraper(site).lower()) # find only words and make them lower case
     
     word_in_text = [validate_word(word) for word in words_index if validate_word(word) is not None]
     
@@ -70,11 +84,13 @@ def word_index(site):
         
     return collections.Counter(word_in_text)
 
+
 def word_scrape_list_of_sites(list_of_sites):
     list_index = [word_index(site) for site in list_of_sites]
+    return list_index
+
+def combine_counters(list_of_counters):
     total_index = collections.Counter()
-    for index in list_index:
-        total_index.update(index)
+    for counter in list_of_counters:
+        total_index.update(counter)
     return total_index
-
-
