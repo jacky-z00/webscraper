467baf0457501db987affa97422af1cd6bee8983
Jacky Zhu <jackyzhu@berkeley.edu>
2017-01-01 14:50:58 -0800

    Able to enter search term and print links of search results

--- Websearcher.py
+++ Websearcher.py
@@ -1,16 +1,28 @@
 
 from googleapiclient.discovery import build
-import pprint
 
 my_api_key = "AIzaSyAbzWZuiWfXtI2tut_t0kc5xK6N1b2QnDM"
 my_cse_id = "005958816095987214880:2wzfznrf0bw"
 
-def google_search(search_term, api_key, cse_id, **kwargs):
+def google_search(search_term, api_key, cse_id, num_results, **kwargs):
+    items = []
+    num_calls = (num_results // 10) + 1
     service = build("customsearch", "v1", developerKey=api_key)
-    res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
-    return res['items']
+    start_index = 1
+    while num_calls > 0:
+        if num_calls == 1:
+            numberofresults = num_results - ((num_results//10) *10)
+            if numberofresults == 0:
+                break
+        else:
+            numberofresults = 10
+        res = service.cse().list(q=search_term, cx=cse_id, num = numberofresults, start = start_index, **kwargs).execute()
+        num_calls -= 1
+        start_index += 10
+        items.extend(res['items'])
+    return items
 
-results = google_search(
-    'stackoverflow site:google.com', my_api_key, my_cse_id, num=10)
+#main
+results = google_search('data', my_api_key, my_cse_id, 10)
 for result in results:
-    pprint.pprint(result)
+    print(result["formattedUrl"])
